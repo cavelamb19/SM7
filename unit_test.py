c2a14ab30691865b5df3c5ddde1d8de645d4f6e7
Daniel Ng <daniel.ng.2019@scis.smu.edu.sg>
2021-11-07 21:16:57 +0800

    new app py and unit test py

--- unit_test.py
+++ unit_test.py
@@ -1,53 +1,59 @@
 import unittest
 from app import Administrator, Employee, Learner, Trainer, Course, Classes, Lesson
-    
- 
+
+
 class TestEmployee(unittest.TestCase):
     def testemployee(self):
-        employee = Employee(StaffID = 1, Name = 'Phris Coskitt', Username = 'csok', Email = 'csoki@gmail.com', 
-        CurrentDesignation = 'administrator', Department ='hr', ContactNo = '90227823')
+        employee = Employee(StaffID = 1, Name = 'Henry loh', Username = 'hen', Email = 'henlow@gmail.com', 
+        CurrentDesignation = 'Hr', Department ='hr', ContactNo = '90227823', Role = 'Administrator')
         self.assertEqual(employee.to_dict(), {
             'StaffID' : 1,
-            'Name' : 'Phris Coskitt',
-            'Username' : 'csok',
-            'Email' : 'csoki@gmail.com',
-            'CurrentDesignation' : 'administrator',
+            'Name' : 'Henry loh',
+            'Username' : 'hen',
+            'Email' : 'henlow@gmail.com',
+            'CurrentDesignation' : 'Hr',
             'Department' : 'hr',
-            'ContactNo' : '90227823'
+            'ContactNo' : '90227823',
+            'Role' : 'Administrator'
         })
+        
+        
 
 class TestLearner(unittest.TestCase):
     def testlearner(self):
-        learner = Learner(StaffID = 3, Name = 'Constance Wilkinson', Username = 'cons', Email = 'cons@gmail.com',
-        CurrentDesignation = 'learner', Department = 'Learning', ContactNo = '92130843', CoursesTaking = 'IS212', CompletedCourses = 'IS214')
+        learner = Learner(StaffID = 3, Name = 'Constance Tan', Username = 'cons', Email = 'constan@gmail.com',
+        CurrentDesignation = 'Engineer', Department = 'Learning', ContactNo = '92130843',CompletedCourses = 'IS214')
         self.assertEqual(learner.to_dict(), {
             'StaffID' : 3,
             'id' : None,
-            'Name' : 'Constance Wilkinson',
+            'Name' : 'Constance Tan',
             'Username' : 'cons',
-            'Email' : 'cons@gmail.com',
-            'CurrentDesignation' : 'learner',
+            'Email' : 'constan@gmail.com',
+            'CurrentDesignation' : 'Engineer',
             'Department': 'Learning',
             'ContactNo' : '92130843',
-            'CoursesTaking' : 'IS212',
             'CompletedCourses' : 'IS214',
+            'CoursesAssigned': None,
+            'CoursesEnrolled': None,
+            'Role': None
                        
         })
 
 class TestTrainer(unittest.TestCase):
     def testtrainer(self):
-        trainer = Trainer(StaffID = 2, Name = 'Arnold de Mari', Username = 'Dr', Email = 'Dr@gmail.com',
-        CurrentDesignation = 'trainer', Department = 'Training', ContactNo = '82329832', coursesTeaching = 'IS212')
+        trainer = Trainer(StaffID = 2, Name = 'Roger Ng', Username = 'Rog', Email = 'Rogng@gmail.com',
+        CurrentDesignation = 'Senior Engineer', Department = 'Training', ContactNo = '82329832', coursesTeaching = 'IS212 Software Project Management G2')
         self.assertEqual(trainer.to_dict(), {
             'StaffID' : 2,
             'id' : None,
-            'Name' : 'Arnold de Mari',
-            'Username' : 'Dr',
-            'Email' : 'Dr@gmail.com',
-            'CurrentDesignation' : 'trainer',
+            'Name' : 'Roger Ng',
+            'Username' : 'Rog',
+            'Email' : 'Rogng@gmail.com',
+            'CurrentDesignation' : 'Senior Engineer',
             'Department': 'Training',
             'ContactNo' : '82329832',
-            'coursesTeaching' : 'IS212'
+            'coursesTeaching' : 'IS212 Software Project Management G2',
+            'Role': None
         })
 
 class TestCourse(unittest.TestCase):
@@ -63,31 +69,36 @@ class TestCourse(unittest.TestCase):
         })
 
 class Testclasses(unittest.TestCase):
-      def testclasses(self):
-          classes= Classes(classesID= 1, startDate="2021-10-18", startTime="2021-10-18 06:30:00", 
-          endDate="2021-11-18", endTime="2021-11-18 06:30:00", classesSize= 50, trainerAssigned="Arnold de Mari", currentEnrolled="0")
-          self.assertEqual(classes.json(), {
-              'classesID' : 1,
-              'startDate' : '2021-10-18',
-              'startTime' : '2021-10-18 06:30:00',
-              'endDate' : '2021-11-18',
-              "endTime" : '2021-11-18 06:30:00',
-              "classesSize" : 50,
-              "trainerAssigned" : 'Arnold de Mari',
-              "currentEnrolled" : '0'
-          })
+    def testclasses(self):
+        classes= Classes(classesID= 1, startDate="August 18 2021", startTime="8am", 
+        endDate="November 5 2021", endTime="12pm", classesSize= 40, trainerAssigned="Roger Ng", currentEnrolled=0)
+        self.assertEqual(classes.json(), {
+            'classesID' : 1,
+            'startDate' : 'August 18 2021',
+            'startTime' : '8am',
+            'endDate' : 'November 5 2021',
+            "endTime" : '12pm',
+            "classesSize" : 40,
+            "trainerAssigned" : 'Roger Ng',
+            "currentEnrolled" : 0
+        })
 
-class Testlesson(unittest.TestCase):
-      def testlesson(self):
-          classes= Lesson(lessonID= 1, courseMaterial="Week1a-Introduction", quizID= 1, 
-          classesID= 1)
-          self.assertEqual(classes.json(), {
-              'lessonID' : 1,
-              'courseMaterial' : 'Week1a-Introduction',
-              'quizID' : 1,
-              'classesID' : 1,
-          })
 
+    def test_IncreaseCurrentEnrolled(self):
+        classes= Classes(classesID= 1, startDate="August 18 2021", startTime="8am", 
+        endDate="November 5 2021", endTime="12pm", classesSize= 40, trainerAssigned="Roger Ng", currentEnrolled=0)
     
-        
-    
+        self.assertEqual(classes.json(), {
+            'classesID' : 1,
+            'startDate' : 'August 18 2021',
+            'startTime' : '8am',
+            'endDate' : 'November 5 2021',
+            "endTime" : '12pm',
+            "classesSize" : 40,
+            "trainerAssigned" : 'Roger Ng',
+            "currentEnrolled" : 0
+        })
+
+        size = classes.IncreaseCurrentEnrolled(2)
+        self.assertEqual(classes.currentEnrolled, 2)
+        #self.assertEqual(size, 4)
