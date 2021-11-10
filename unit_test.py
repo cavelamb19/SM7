a38aab0e5b90e5444e517bf7edadf66268a85b63
qisheng97 <72507713+qisheng97@users.noreply.github.com>
2021-11-10 16:35:27 +0800

    Update unit_Test.py

--- unit_test.py
+++ unit_test.py
@@ -1,5 +1,7 @@
+from operator import le
 import unittest
-from app import Administrator, Employee, Learner, Trainer, Course, Classes, Lesson
+from SPM7.app import lesson
+from app import Administrator, Employee, Learner, Trainer, Course, Classes, Lesson, Quiz, Quizscore, QuestionTrueFalse
  
 #Daniel
 class TestEmployee(unittest.TestCase):
@@ -106,3 +108,68 @@ class Testclasses(unittest.TestCase):
         size = classes.IncreaseCurrentEnrolled(2)
         self.assertEqual(classes.currentEnrolled, 2)
         #self.assertEqual(size, 4)
+        
+#Daniel      
+class TestLesson(unittest.TestCase):
+
+  def testclasses(self):
+      lesson = Lesson(lessonID=1, courseMaterial="Week1a-Introduction",classesID=1)
+
+      self.assertEqual(lesson.json(), {
+        'lessonID': 1,
+        'courseMaterial': 'Week1a-Introduction',
+        'classesID': 1
+     
+    })
+      
+#Qi Sheng  
+class TestQuiz(unittest.TestCase):
+
+  def testquiz(self):
+      
+      quiz = Quiz(quizID=1, quizDuration="15 mins",attemptNo="Unlimited",quizTitle="Week1 Quiz", quizDesc="No calculator", lessonID=1)
+
+      self.assertEqual(quiz.json(), {
+        'quizID': 1,
+        'quizDuration': '15 mins',
+        'attemptNo': 'Unlimited',
+        'quizTitle':"Week1 Quiz",
+        "quizDesc" : "No calculator",
+        "lessonID":1
+     
+    })
+      
+#Bernice     
+class TestQuestionTrueFalse(unittest.TestCase):
+
+  def testQuestionTrueFalse(self):
+
+      question = QuestionTrueFalse(
+          qnID=1, qn="Software Project Management module is about coding" , ans="False",quizID=1)
+
+      self.assertEqual(question.json(), {
+          'qnID': 1,
+          'qn': "Software Project Management module is about coding",
+          'ans': "False",
+          'quizID': 1,
+
+
+      })
+      
+#Terence
+class TestQuizscore(unittest.TestCase):
+    
+  def testQuizscore(self):
+      
+      quizscore = Quizscore(qsID=1, quizscore=0 ,quizID=1,learnerID=3)
+
+      self.assertEqual(quizscore.json(), {
+          'qsID': 1,
+          'quizscore': 0,
+          'quizID': 1,
+        'learnerID':3,
+      
+     
+    })
+          
+
